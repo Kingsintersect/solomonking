f4b59431245e5ce78a6e62eb1de23ed59ffcdcd5
kingsintersect <kingsintersect@gmail.com>
2023-08-11 04:27:45 +0100

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './portfolio/components/home/home.component';
+import { TestimonialsComponent } from './portfolio/components/testimonials/testimonials.component';
+import { ContactmeComponent } from './portfolio/components/contactme/contactme.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { AboutComponent } from './portfolio/components/about/about.component';
+import { ResumeComponent } from './portfolio/components/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TestimonialsComponent,
+    ContactmeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
